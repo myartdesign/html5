8947f00efdaa56dd35255c7bb659f4143cd7cb44
myartdesign <40593038+myartdesign@users.noreply.github.com>
2019-09-06 01:40:52 -0300

    Update phaser-capture.js

--- baby_abby_funny_crafting_day/plugins/phaser-capture.js
+++ baby_abby_funny_crafting_day/plugins/phaser-capture.js
@@ -1,1 +1,37 @@
-
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    }
+    else if (typeof exports === 'object') {
+        module.exports = factory();
+    }
+    else {
+        root.Phaser.Plugin.Capture = factory();
+    }
+}(this, function () {
+    function Capture(game, parent) {
+        Phaser.Plugin.call(this, game, parent);
+        this.pendingScreenshot = null;
+        this.game = game;
+    };
+    Capture.prototype.screenshot = function (callback, options) {
+        if (!callback) {
+            return;
+        }
+        options = options || {}
+        this.pendingScreenshot = {
+            format: options.format || 'image/png'
+            , callback: callback
+        , }
+    };
+    Capture.prototype.postRender = function () {
+        if (this.pendingScreenshot) {
+            var dataUrl = game.canvas.toDataURL(this.pendingScreenshot.format);
+            if (this.pendingScreenshot.callback) {
+                this.pendingScreenshot.callback(dataUrl);
+                this.pendingScreenshot = null;
+            }
+        }
+    };
+    return Capture;
+}));
